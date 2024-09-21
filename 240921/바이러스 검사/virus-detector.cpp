3fa35383748884eb2084ecfa00fe6f8c26039a51
whxodus0121 <92247333+whxodus0121@users.noreply.github.com>
2024-09-21 21:04:55 +0900

    Title: 바이러스 검사, Time: 489ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 240921/바이러스 검사/virus-detector.cpp
+++ 240921/바이러스 검사/virus-detector.cpp
@@ -3,35 +3,30 @@ using namespace std;
 
 int n;
 int res[1000000];
-int leader,slave;
+int leader, slave;
 
 int main() {
-    // 여기에 코드를 작성해주세요.  
+    int ans = 0;
+    cin >> n;
 
-    int ans=0;
-    cin>>n;
-
-    for(int i=0;i<n;i++){
-        cin>>res[i];
+    for(int i = 0; i < n; i++) {
+        cin >> res[i];
     }
-    cin>>leader>>slave;
+    cin >> leader >> slave;
 
-    for(int i=0;i<n;i++){
-        res[i]=res[i]-leader;
-        ans+=1;
-        if(res[i]>0){
-            
+    for(int i = 0; i < n; i++) {
+        res[i] = res[i] - leader;
+        ans += 1;
+        if(res[i] > 0) {
             if(res[i] % slave == 0) {
-            ans += (res[i] / slave);
-            } 
-            else {
+                ans += (res[i] / slave);
+            } else {
                 ans += (res[i] / slave + 1);
             }
         }
     }
 
-    cout<<ans;
-
+    cout << ans;
 
     return 0;
 }
